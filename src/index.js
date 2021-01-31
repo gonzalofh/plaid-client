9bf0667743cf85b58005ea4da743f03f0babb47d
gonzalofh <gonzalofh@users.noreply.github.com>
2021-01-31 18:40:42 -0500

    fixed bug when fetching env variables

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 
 import axios from 'axios';
 
-const host = process.env("PLAID_HOST");
-const clientId = process.env("PLAID_CLIENT_ID");
-const secretKey = process.env("PLAID_SECRET_KEY")
+const host = process.env["PLAID_HOST"];
+const clientId = process.env["PLAID_CLIENT_ID"];
+const secretKey = process.env["PLAID_SECRET_KEY"];
 
 exports.getAccount = (accessToken, responseType) => {
 	return axios({
